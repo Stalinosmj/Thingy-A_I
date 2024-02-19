3850ee69374760cda1c807e85a8f183bb8394731
Stalino A J <93860427+Stalinosmj@users.noreply.github.com>
2024-02-19 16:01:59 +0530

    added blurry loading

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:thingy/database/api_setup.dart';
+import 'package:blurry_modal_progress_hud/blurry_modal_progress_hud.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -14,78 +15,100 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   File? _selectedImage;
   String? selectedAnswer;
+  bool _loading = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: const Text("Thingy AI - Prototype"),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextButton(
-              onPressed: () {
-                pickImageFromGallery();
-              },
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.blue,
-              ),
-              child: const Text(
-                "Pick an Image",
-                style: TextStyle(
-                  color: Colors.white,
+    return BlurryModalProgressHUD(
+      inAsyncCall: _loading,
+      blurEffectIntensity: 4,
+      dismissible: false,
+      opacity: 0.4,
+      color: Colors.cyanAccent,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: const Text("Thingy AI - Prototype"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () {
+                  pickImageFromGallery();
+                },
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                ),
+                child: const Text(
+                  "Pick an Image",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            _selectedImage != null
-                ? Image.file(
-              _selectedImage!,
-              fit: BoxFit.contain,
-              width: 250,
-            )
-                : const Text("Please select an Image"),
-            const SizedBox(
-              height: 20,
-            ),
-            _selectedImage != null
-                ? TextButton(
-              onPressed: () async {
-                if (_selectedImage != null) {
-                  selectedAnswer = await askAI();
-                  setState(() {});
-                }
-              },
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.blue,
+              const SizedBox(
+                height: 30,
               ),
-              child: const Text(
-                "Get Questions",
-                style: TextStyle(color: Colors.white),
+              _selectedImage != null
+                  ? Image.file(
+                      _selectedImage!,
+                      fit: BoxFit.contain,
+                      width: 250,
+                    )
+                  : const Text("Please select an Image"),
+              const SizedBox(
+                height: 20,
               ),
-            )
-                : const Text("Pick An Image First"),
-            const SizedBox(
-              height: 25,
-            ),
-            selectedAnswer != null
-                ? Container(
-              color: Colors.blue[100],
-              height: 200,
-              width: MediaQuery.of(context).size.width * 0.8,
-              child: Expanded(
-                  child: SingleChildScrollView(
-                    scrollDirection: Axis.vertical,
-                    child: Text(selectedAnswer!),
-                  )),
-            )
-                : const Text("Click on Generate Questions"),
-          ],
+              _selectedImage != null
+                  ? TextButton(
+                      onPressed: () async {
+                        if (_selectedImage != null) {
+                          selectedAnswer = await askAI();
+                          setState(() {});
+                        }
+                      },
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                      ),
+                      child: const Text(
+                        "Who are You?",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    )
+                  : const Text("Pick An Image First"),
+              const SizedBox(
+                height: 25,
+              ),
+              selectedAnswer != null
+                  ? Container(
+                      color: Colors.blue[100],
+                      height: 200,
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: Column(
+                        children: <Widget>[
+                          Expanded(
+                            child: SingleChildScrollView(
+                              scrollDirection: Axis.vertical,
+                              child: Text(selectedAnswer!),
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text("Proceed"),
+                            style: TextButton.styleFrom(
+                              foregroundColor: Colors.white,
+                              backgroundColor: Colors.blue,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  : const Text("Click on Pick an Image"),
+            ],
+          ),
         ),
       ),
     );
@@ -93,13 +116,16 @@ class _HomePageState extends State<HomePage> {
 
   Future pickImageFromGallery() async {
     final returnedImage =
-    await ImagePicker().pickImage(source: ImageSource.gallery);
+        await ImagePicker().pickImage(source: ImageSource.gallery);
     setState(() {
       _selectedImage = File(returnedImage!.path);
     });
   }
 
   Future askAI() async {
+    setState(() {
+      _loading = true;
+    });
     final imageAsBytes = await _selectedImage!.readAsBytes();
     final prompt = TextPart(
         "Consider the main object in this image as yourself ,now tell something about yourself");
@@ -107,6 +133,11 @@ class _HomePageState extends State<HomePage> {
     final response = await model.generateContent([
       Content.multi([prompt, ...imagePart])
     ]);
+
+    setState(() {
+      _loading = false;
+    });
+
     return response.text;
   }
 }
